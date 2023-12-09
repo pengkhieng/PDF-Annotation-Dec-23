df5aa52f9e5d20a23fccd7ef2189f612edd5fdaf
pengkhieng kim <khieng.mantisy@gmail.com>
2023-12-10 00:09:04 +0700

    Initial Commit

---
+++ PDF-Annotation/PDF_AnnotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_AnnotationApp.swift
+//  PDF-Annotation
+//
+//  Created by Pengkhieng Kim on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PDF_AnnotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
